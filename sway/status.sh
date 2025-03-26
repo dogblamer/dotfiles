cc0538fb077f745ba23746fa28310b783e5d151d
dogblamer <dogblamer@tuta.com>
2025-03-26 21:40:54 +0100

    feat: added helix and changed some other dotfiles

--- sway/status.sh
+++ sway/status.sh
@@ -2,25 +2,16 @@
 
 # shows time
 date=$(date +'%X')
-
 # shows active window in swaybar, if app_id == null, than the name will be selected.
 # https://packages.gentoo.org/packages/app-misc/jq
 windowtitle=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).app_id' 2>/dev/null)
 windowtitle_name=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).name' 2>/dev/null)
-windowtitle_ws=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).name' 2>/dev/null)
-NUM=$"5"
-NUMF=$=[$NUM+0]
+volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1)
 
 # shows ".name"
-# checks if windowtitle is a workspace number
-for NUM in {1..10}; do
-	if [ "$windowtitle_ws" == NUM ] ; then
-		echo "| $date "
-	# shows ".name"
-	elif [ "$windowtitle" == 'null' ]; then
-		echo "$windowtitle_name | $date "
-	# shows ".app.id"
-	else
-		echo "$windowtitle | $date "
-	fi
-done
+if [ "$windowtitle" == 'null' ]; then
+	echo "$windowtitle_name | $volume % | $date "
+# shows ".app.id"
+else
+	echo "$windowtitle | $volume % | $date "
+fi
